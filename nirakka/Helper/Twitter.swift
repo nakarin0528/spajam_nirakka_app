c9aa707337a080b70e97735ce8eb3baf2f5f4331
UdonDa <udoooon0727@gmail.com>
2018-05-27 03:23:35 +0900

    [Add]tweet func

--- nirakka/Helper/Twitter.swift
+++ nirakka/Helper/Twitter.swift
@@ -1,9 +1,30 @@
-//
-//  Twitter.swift
-//  nirakka
-//
-//  Created by daichi on 2018/05/27.
-//  Copyright © 2018年 teamNirakka. All rights reserved.
-//
+import TwitterKit
 
-import Foundation
+class TwitterUtils {
+    
+    let shareURL = "https://spajam.jp/"
+    
+    func tweet(team: String, yen:Int, completion: @escaping () -> Void) {
+        //ユーザー情報があるかどうか調べる
+        if let userID = TWTRTwitter.sharedInstance().sessionStore.session()?.userID {
+            let client = TWTRAPIClient(userID: userID)
+            var clientError : NSError?
+            let endPoint = "https://api.twitter.com/1.1/statuses/update.json"
+            let text = "\n\(team)円を\(yen)に寄付しました. #tipay #\n\(shareURL)"
+            let params = ["status" : text]
+            
+            let request = client.urlRequest(withMethod: "POST", urlString: endPoint, parameters: params, error: &clientError)
+            
+            client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+                if (connectionError == nil) {
+                    print("sucess");
+                    completion()
+                }
+                else {
+                    print(String(describing: connectionError))
+                }
+            }
+        }
+    }
+
+}
