47147ae24afaa2fe6e8f22efd8db11f8aaece74f
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2018-05-26 14:02:20 +0900

    fix build error

---
+++ nirakka/Class/View/StartVC.swift
@@ -0,0 +1,23 @@
+//
+//  StartVC.swift
+//  nirakka
+//
+//  Created by Iko Nakari on 2018/05/26.
+//  Copyright © 2018年 teamNirakka. All rights reserved.
+//
+
+import UIKit
+import Material
+import SnapKit
+
+final class StartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
